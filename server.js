e18acb8600d4365c379f753dee99733dc21ae75e
Kelly Fragkogianni <kfragkog@Kellys-MacBook-Pro.local>
2023-01-25 14:58:49 +0000

    DB setup

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan'); //a logger npm package;
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars from config file
 dotenv.config({ path: './config/config.env' });
+
+// Connect to db
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -21,7 +26,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 // That's how we access env variables with dotenv (after we have loaded them as per above)
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
