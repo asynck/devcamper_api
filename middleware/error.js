94fffaa6f14191db27794024cd59222a2984442f
Kelly Fragkogianni <kfragkog@Kellys-MacBook-Pro.local>
2023-02-01 16:56:23 +0000

    Added extra validations to error handler

--- middleware/error.js
+++ middleware/error.js
@@ -7,12 +7,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for devs - stack (and message on line 11) is a property of err object passed in here
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Bootcamp not found with id ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Bootcamp name already exists`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
