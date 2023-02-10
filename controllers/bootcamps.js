0839e3221f59facb175c2c21a4ff918047e5ce4c
Kelly Fragkogianni <kfragkog@Kellys-MacBook-Pro.local>
2023-02-10 16:55:13 +0000

    Finished seeder and get all bootcamps by radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,9 @@
 const ErrorResponse = require('../utils/errorResponse');
+const geoCoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 // Using asyncHandler instead of try catch for each error that can be returned in each method
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 
 // @desc    Get all bootcamps
 // @route   GET to /api/v1/bootcamps
@@ -85,5 +87,34 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     success: true,
     data: bootcamp,
     message: `Deleted bootcamp ${req.params.id}`,
+  });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET to /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  // Get zipcode and distance from that zipcode, from the url params
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide distance by earth radius
+  // Earth radius = 3,963 miles
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+    message: `All bootcamps within radius`,
   });
 });
--- seeder.js
+++ seeder.js
@@ -22,13 +22,30 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
-// Import into DB
+// Import data into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    console.log('Data imported...'.yellow.inverse);
+    console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (error) {
     console.error(error);
   }
 };
+
+// Delete all data from DB
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data deleted...'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
